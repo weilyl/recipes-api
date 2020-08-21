695b3ae5fc869bd4f192455c57f6048bdf0ca0c6
weilyl <weily.lang@gmail.com>
2020-08-21 01:36:45 -0400

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,16 @@ user = User.create(username: "test2", password: "test2")
 # "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.oT7kSePnYs7eVIsRIzIi0UEC7XBclsrO3qrnXwic8Zg"
 
 =begin
-Heroku new user
-{
     "username": "iusethis",
     "password": "thispword"
-}
 "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyfQ.4aV9sPAQmgK4hTBPihEXF3CVkzLDz3jsmWShy2TtQfU"
+
+"username": "thisisuser1",
+"password": "thispword1"
+token: eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjozfQ.ocFaRU1kYVGQ6vTH0wuz5Wn_HcmUbBzB6mlcgqoUcho
+
+
+"username": "thisisuser2",
+"password": "thispword2"
+eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0fQ.3BbRA0X8L4RCsF8-r4w6HrL4hFQKeGz6OQ522OyH57I
 =end
